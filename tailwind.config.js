e7996944b4bcf8a3c5e671979d7a58ee37af55dd
peterleunan1 <arpm081001@gmail.com>
2023-09-14 15:18:30 -0500

    feat: change app title and google font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#1db954',
+        accentPrimary: '#064d1f',
+        text: '#535353',
+        bg: '#fff',
+      },
+      fontFamily: {
+        signika: ['Signika', 'sans-serif'],
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
 }
-
